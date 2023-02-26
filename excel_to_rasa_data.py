39eadfc93c1c50a63117f721f9b89ba1286cfc06
Xia Qiang <huakaijianwo2009@163.com>
2023-02-26 15:28:50 +0800

    add rules

--- excel_to_rasa_data.py
+++ excel_to_rasa_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pandas as pd
 
 
-def create_rasa_files(nlu_path, domain_path, excel_path):
+def create_rasa_files(nlu_path, domain_path, rule_path,  excel_path):
     # Load data from Excel file
     data = pd.read_excel(excel_path)
 
@@ -13,9 +13,10 @@ def create_rasa_files(nlu_path, domain_path, excel_path):
 
     domain_file = open(domain_path, 'w', encoding='utf-8')
     nlu_file = open(nlu_path, 'w', encoding='utf-8')
-
+    rule_file = open(rule_path, 'w', encoding='utf-8')
     domain_file.write("versin: '3.1'\n\nintents:\n")
     nlu_file.write("version: '3.1'\n\nnlu:\n")
+    rule_file.write("version: '3.1'\n\nrules:\n")
     # Loop through data and add to dictionary
     for _, row in data.iterrows():
         intent_name = row['Intent']
@@ -28,10 +29,12 @@ def create_rasa_files(nlu_path, domain_path, excel_path):
 
     for intent_name in intents.keys():
         domain_file.write(f"  - {intent_name}\n")
+        rule_file.write(f" - rule: {intent_name}\n   steps:\n    - intent:{intent_name}\n    - action:utter_{intent_name}\n\n")
+    domain_file.write("")
     domain_file.write(f"\nresponses:\n")
     for intent_name, questions in intents.items():
 
-        domain_file.write(f"  {intent_name}:\n     - text: {responses[intent_name]}\n\n")
+        domain_file.write(f"  utter_{intent_name}:\n     - text: {responses[intent_name]}\n\n")
 
         nlu_file.write(f"- intent: {intent_name}\n  examples: |\n")
         nlu_file.write(f"    - {question}\n\n")
@@ -41,4 +44,5 @@ if __name__ == '__main__':
     excel_path = "data/痛风问答.xlsx"
     nlu_path = "data/nlu.yml"
     domain_path = "data/domain.yml"
-    create_rasa_files(nlu_path, domain_path, excel_path)
+    rule_file = "data/rules.yml"
+    create_rasa_files(nlu_path, domain_path,rule_file, excel_path)
